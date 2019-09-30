e63a58d4c26afa0f6504f8d1f3b8f107c26f5624
Happi Yvan <nwaforhappiyvan@gmail.com>
2019-09-30 11:12:11 +0100

    Program to check if a given number in range 40 ... 10000 presents in two number (in same range).
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ w3resource/Basic-JS/js-program-47.js
@@ -0,0 +1,24 @@
+// ######################################################################################################
+// #                                                                                                    #
+// #    Program Purpose: Checks if a given number in the range 40....100000 presents in two number (in
+// #                     same range).
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                          #
+// #       Program Date: September 30, 2019.                                                            #
+// #                                                                                                    #
+// ######################################################################################################
+
+let checkCondition = (lower, val, upper) => {
+    if (val < 40 || val > 10000)
+        return false;
+    else
+        return val >= lower && val <= upper;
+};
+
+let runTests = () => {
+    console.log(checkCondition(40, 45, 4000));
+    console.log(checkCondition(80, 79, 320));
+    console.log(checkCondition(89, 30, 4000));
+};
+
+// Testing the program...
+runTests();
