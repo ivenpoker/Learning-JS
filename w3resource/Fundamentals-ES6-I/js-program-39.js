37300bd66280673bfac6c6522098de506d1a8f64
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-12 16:29:10 +0100

    Program that removes the key-value pairs corresponding to the given keys from an object.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ w3resource/Fundamentals-ES6-I/js-program-39.js
@@ -0,0 +1,30 @@
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Removes the key-value pairs corresponding to the given keys from an object.     #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: February 10, 2020.                                                              #
+// #                                                                                                     #
+// #######################################################################################################
+
+const removeKeyPair = (obj = {}, keys = []) =>
+    Object.keys(obj).filter((key) => !keys.includes(key)).reduce((acc, key) =>
+        ({
+            ...acc,
+            [key]: obj[key]
+        }), {});
+
+const runTest = () => {
+    const person = {
+        firstName: 'James',
+        lastName: 'King',
+        userName: 'deathshot',
+    };
+    console.log('Original Object => ', person);
+    console.log("Removing 'firstName' => ", removeKeyPair(person, ['firstName']));
+    console.log("Removing 'lastName'  => ", removeKeyPair(person, ['lastName']));
+    console.log("Removing 'userName'  => ", removeKeyPair(person, ['userName']));
+
+    console.log("\nFinal 'person' object:", person);
+};
+
+runTest();
