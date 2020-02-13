2406af415331bb91ad96f17dd0718485f22504f9
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-13 11:01:04 +0100

    Program that adds special characters to text to print to color in the console (combined with console.log()).
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ w3resource/Fundamentals-ES6-I/js-program-65.js
@@ -0,0 +1,35 @@
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Adds special characters to text to print in color in the console (combined with
+// #                     console.log()).
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: February 13, 2020.                                                              #
+// #                                                                                                     #
+// #######################################################################################################
+
+const colorize = (...args) => ({
+    black: `\x1b[30m${args.join(' ')}`,
+    red: `\x1b[31m${args.join(' ')}`,
+    green: `\x1b[32m${args.join(' ')}`,
+    yellow: `\x1b[33m${args.join(' ')}`,
+    blue: `\x1b[34m${args.join(' ')}`,
+    magenta: `\x1b[35m${args.join(' ')}`,
+    cyan: `\x1b[36m${args.join(' ')}`,
+    white: `\x1b[37m${args.join(' ')}`,
+    bgBlack: `\x1b[40m${args.join(' ')}\x1b[0m`,
+    bgRed: `\x1b[41m${args.join(' ')}\x1b[0m`,
+    bgGreen: `\x1b[42m${args.join(' ')}\x1b[0m`,
+    bgYellow: `\x1b[43m${args.join(' ')}\x1b[0m`,
+    bgBlue: `\x1b[44m${args.join(' ')}\x1b[0m`,
+    bgMagenta: `\x1b[45m${args.join(' ')}\x1b[0m`,
+    bgCyan: `\x1b[46m${args.join(' ')}\x1b[0m`,
+    bgWhite: `\x1b[47m${args.join(' ')}\x1b[0m`
+});
+
+const runTests = () => {
+    console.log(colorize('foo').red);
+    console.log(colorize('foo', 'bar').bgBlue);
+    console.log(colorize(colorize('foo').yellow, colorize('foo').green).bgWhite);
+};
+
+runTests();
