63ed5e3e7626831f1bdb6c6c560d64bca430870a
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-17 12:26:29 +0100

    Program that removes an event listener from an element.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ w3resource/Fundamentals-ES6-I/js-program-93.js
@@ -0,0 +1,21 @@
+
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Removes an event listener from an element.                                      #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: February 15, 2020.                                                              #
+// #                                                                                                     #
+// #######################################################################################################
+
+const offListener = (elem = {}, evt, callback, opts = false) =>
+    elem.removeEventListener(evt, callback, opts);
+
+(() => {
+
+    const newElem = document.createElement('h1');
+    newElem.addEventListener('click', () => console.log('heading clicked'));
+    console.log(offListener(newElem, 'click', () => {
+        console.log('Just switched off the click listener for h1');
+    }))
+
+})();
