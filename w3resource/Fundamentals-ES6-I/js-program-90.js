2fbe976279f73a2155a3bb37381e4661c0312077
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-17 10:29:10 +0100

    Program that demonstrates how to do nested comments (like those in reddit.com). Given flat array of objects linked to one another, program nests them recursively.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ w3resource/Fundamentals-ES6-I/js-program-90.js
@@ -0,0 +1,30 @@
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: JavaScript program to nest a given flat array of objects linked to one another  #
+// #                     recursively.                                                                    #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: February 15, 2020.                                                              #
+// #                                                                                                     #
+// #######################################################################################################
+
+const nest = (items, id = null, link = 'parent_id') =>
+    items.filter((item) => item[link] === id)
+         .map((item) => ({
+             ...item,
+             children: nest(items, item.id)
+         }));
+
+(() => {
+
+    const comments = [
+        {id: 1, parent_id: null},
+        {id: 2, parent_id: 1},
+        {id: 3, parent_id: 1},
+        {id: 4, parent_id: 2},
+        {id: 5, parent_id: 4},
+    ];
+
+    const nestedComments = nest(comments);
+    console.log(nestedComments);
+
+})();
