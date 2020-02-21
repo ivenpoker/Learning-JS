adedb131d8bb22db323301b3a9fbda2e1f91d989
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-02-21 09:01:24 +0100

    Program that converts a string to kebab case.

---
+++ w3resource/Fundamentals-ES6-I/js-program-123.js
@@ -0,0 +1,24 @@
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Converts a string to kebab case.                                                #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: February 21, 2020.                                                              #
+// #                                                                                                     #
+// #######################################################################################################
+
+const toKebabCase = (str) =>
+    str &&
+    str
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map(x => x.toLowerCase())
+        .join('-');
+
+(() => {
+
+    console.log(toKebabCase('camelCase'));
+    console.log(toKebabCase('some text'));
+    console.log(toKebabCase('some-mixed_string With spaces_underscores-and-hyphens'));
+    console.log(toKebabCase('AllThe-small Things'));
+    console.log(toKebabCase('IAmListeningToFMWhileLoadingDifferentURLOnMyBrowserAndAlsoEditingSomeXMLAndHTML'));
+
+})();
