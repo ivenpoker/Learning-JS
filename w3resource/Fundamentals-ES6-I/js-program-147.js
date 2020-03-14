9b033efd41a487f917f1f723ab05b4ae2538e8e8
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-03-14 20:52:10 +0100

    Program that serializes a cookie name-value pair into a Set-cookie header string.

---
+++ w3resource/Fundamentals-ES6-I/js-program-147.js
@@ -0,0 +1,18 @@
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Serializes a cookie name-value pair into a Set-cookie header string.            #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: March 14, 2020.                                                                 #
+// #                                                                                                     #
+// #######################################################################################################
+
+
+const serializeCookie = (name, val) =>
+    `${encodeURIComponent(name)}=${encodeURIComponent(val)}`;
+
+(function () {
+
+    console.log("Seriallized data:", serializeCookie("foo", "bar"));
+
+})();
+
