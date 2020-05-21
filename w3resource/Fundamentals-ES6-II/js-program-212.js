8eb923d754c2b43b851f289a3ebcb4d45b3324f4
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-05-21 14:33:48 +0100

    Update for previous program (previous commit)

--- w3resource/Fundamentals-ES6-II/js-program-212.js
+++ w3resource/Fundamentals-ES6-II/js-program-212.js
@@ -7,11 +7,14 @@
 // #######################################################################################################
 
 const createNDimension = (nVal) => Array(nVal).fill(0);
+const initializeNDArray = (val, ...args) =>
+	args.length === 0
+		? val
+		: Array.from({length: args[0]}).map(() => initializeNDArray(val, ...args.slice(1)));
 
 (function () {
 
-	console.log("Array of 5:", createNDimension(5));
-	console.log("Array of 2:", createNDimension(2));
-	console.log("Array of 10:", createNDimension(10));
+	console.log(initializeNDArray(1, 3));
+	console.log(initializeNDArray(5, 2, 2, 2));
 
 })();
