f437007787d5f0f47405d81cdc6586645e10e5b4
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-06-05 12:39:43 +0100

    Program that checks if a given number falls within a certain range.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

---
+++ w3resource/Fundamentals-ES6-II/js-program-217.js
@@ -0,0 +1,19 @@
+
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Checks if a given number falls within a certain range.						     #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: June 05, 2020.                                                                  #
+// #                                                                                                     #
+// #######################################################################################################
+
+const inRange = (lower, val, upper) => lower <= val && val <= upper;
+
+(function () {
+
+	console.log(inRange(3, 2, 5));
+	console.log(inRange(3, 4));
+	console.log(inRange(2, 3, 5));
+	console.log(inRange(3, 2))
+
+})();
