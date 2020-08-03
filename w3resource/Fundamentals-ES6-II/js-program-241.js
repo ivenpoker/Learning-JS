13cf37cc71e6379ff25aa93a96b839a115a880e9
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-08-03 23:16:24 +0100

    Program that removes element in an array until the passed function returns true. Returns the remaining elements in an array.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- w3resource/Fundamentals-ES6-II/js-program-241.js
+++ w3resource/Fundamentals-ES6-II/js-program-241.js
@@ -1,15 +1,29 @@
 // #######################################################################################################
 // #                                                                                                     #
-// #    Program Purpose: Returns true if the parent element contains the child element, false otherwise. #
+// #    Program Purpose: Removes elements in an array until the passed function returns true. Returns    #
+// #					 the remaining elements in the array.										     #
 // #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
-// #       Program Date: June 26, 2020.                                                                  #
+// #       Program Date: August 03, 2020.                                                                #
 // #                                                                                                     #
 // #######################################################################################################
 
-
+const doProcessing = (data = [], func=f=>f) => {
+	try {
+		data.forEach((val, idx) => {
+			if (func(val)) {
+				throw idx;
+			}
+		});
+	} catch (idx) {
+		// console.log("idx:", idx)
+		return data.slice(idx)
+	}
+}
 
 (function () {
 
-
+	const tempData = [1, 3, 19, 5, 7, 2, 17, 13, 23];
+	const results = doProcessing(tempData, (val) => val % 2 === 0);
+	console.log(results)
 
 })();
