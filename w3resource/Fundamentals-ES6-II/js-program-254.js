bed5456d81950c45cc6b446fe70dc4b8b56a7527
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-08-05 17:22:04 +0100

    Program that generates a UUID in Node.JS.

---
+++ w3resource/Fundamentals-ES6-II/js-program-254.js
@@ -0,0 +1,20 @@
+// #######################################################################################################
+// #                                                                                                     #
+// #    Program Purpose: Generate a UUID in Node.JS.												 	 #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                           #
+// #       Program Date: August 05, 2020.                                                                #
+// #                                                                                                     #
+// #######################################################################################################
+
+const crypto = require("crypto");
+
+const UUIDGeneratorNode = () =>
+	([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+		(c ^ (crypto.randomBytes(1)[0] & (15 >> (c / 4)))).toString(16)
+	);
+
+(function () {
+
+	console.log("Node crypto UUID:", UUIDGeneratorNode());
+
+})();
