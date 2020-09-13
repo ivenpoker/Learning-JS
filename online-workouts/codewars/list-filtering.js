d615f35b9616f8d01738041f4d4eee1a9668f90c
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-09-13 17:58:50 +0100

    [CODE_WARS]: Program that finds sum of odd numbers from specific index.

--- online-workouts/codewars/list-filtering.js
+++ online-workouts/codewars/list-filtering.js
@@ -1,36 +1,17 @@
 // ######################################################################################################
 // #                                                                                                    #
-// #    Program Purpose: Sum of odd numbers from specific index.                                        #
+// #    Program Purpose: Filter out specific data from array of data.                                   #
 // #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                          #
 // #       Program Date: September 13, 2020.                                                            #
 // #                                                                                                    #
 // ######################################################################################################
 
-const rowSumOddNumbers = (num) => {
-	if (num <= 1) {
-		return 1;
-	} else {
-		// rebuild triangle up to th 'numTh' element
-		let data = [[1]];
-		for (let i = 2; i <= num; i++) {
-			let temp = data[data.length-1]
-			let lastOdd = temp[temp.length-1];
-
-			const tempArray = []
-			for (let j = 0; j < i; j++) {
-				tempArray.push(lastOdd + 2);
-				lastOdd += 2;
-			}
-			data = [...data, tempArray]
-		}
-
-		// Sum specific row data
-		return data[num-1].reduce((acc, val) => acc + val, 0);
-	}
-}
+const filterList = (data = []) =>
+	data.filter((val) => !(typeof(val) == "string"));
 
 (function () {
 
-	console.log(rowSumOddNumbers(1))
+	console.log(filterList([1, 2, 'a', 'b']));
+	console.log(filterList([1, 'a', 'b', 0, 15]))
 
 })();
