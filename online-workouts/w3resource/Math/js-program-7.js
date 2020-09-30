11c6e9b8ff83a423d71d422fa63a9ef98ac5555e
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-09-30 11:58:33 +0100

    [W3R]: Program that finds the lowest value in array.

---
+++ online-workouts/w3resource/Math/js-program-7.js
@@ -0,0 +1,17 @@
+// ######################################################################################################
+// #                                                                                                    #
+// #    Program Purpose: Finds the lowest value in array.						                        #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                          #
+// #       Program Date: September 30, 2020.                                                            #
+// #                                                                                                    #
+// ######################################################################################################
+
+(function () {
+
+	const dataA = [12, 34, 56, 1];
+	const dataB = [-12, -34, 0,-56, -1];
+
+	console.log("Min:", Math.min(...dataA));
+	console.log("Min:", Math.min(...dataB));
+
+})();
