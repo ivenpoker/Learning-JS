0f3a4d26ef17c4fa81ccb7e22eb8b1a7492ce061
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-09-30 11:56:17 +0100

    Program that finds the highest value in array.

---
+++ online-workouts/w3resource/Math/js-program-6.js
@@ -0,0 +1,17 @@
+// ######################################################################################################
+// #                                                                                                    #
+// #    Program Purpose: Finds the highest value in array.						                        #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                          #
+// #       Program Date: September 30, 2020.                                                            #
+// #                                                                                                    #
+// ######################################################################################################
+
+(function () {
+
+	const dataA = [12, 34, 56, 1];
+	const dataB = [-12, -34, 0,-56, -1];
+
+	console.log("Max:", Math.max(...dataA));
+	console.log("Max:", Math.max(...dataB));
+
+})();
