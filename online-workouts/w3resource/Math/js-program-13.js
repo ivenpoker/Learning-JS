e1492e04c19500286ba8f04e7c8fc6cc9e6a06da
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-10-25 12:19:02 +0100

    [W3R]: Program that determines if a number is a power of 2 or not.

---
+++ online-workouts/w3resource/Math/js-program-13.js
@@ -0,0 +1,22 @@
+// ######################################################################################################
+// #                                                                                                    #
+// #    Program Purpose: Determines if a number is a power of 2 or not.                                 #
+// #     Program Author: Happi Yvan <ivensteinpoker@gmail.com>                                          #
+// #       Program Date: October 30, 2020.                                                              #
+// #                                                                                                    #
+// ######################################################################################################
+
+const isNumberAPowerOfTwo = (someNum) => {
+    if (Number.isNaN(someNum) || !Number.isFinite(someNum)) {
+        return false;
+    }
+    return Math.sqrt(someNum) % 2 === 0;
+}
+
+(function () {
+
+    console.log(isNumberAPowerOfTwo(16));
+    console.log(isNumberAPowerOfTwo(18));
+    console.log(isNumberAPowerOfTwo(256));
+
+})();
