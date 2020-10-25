1ef78c08c56b9c68610f1a72699a3170d70dfce6
Happi Yvan <nwaforhappiyvan@gmail.com>
2020-10-25 12:34:02 +0100

    [W3R]: Add alternative solution to previous program.
    Signed-off-by: Happi Yvan <nwaforhappiyvan@gmail.com>

--- online-workouts/w3resource/Math/js-program-15.js
+++ online-workouts/w3resource/Math/js-program-15.js
@@ -6,16 +6,32 @@
 // #                                                                                                    #
 // ######################################################################################################
 
-const isIntegerValue = (someValue) => {
+const isIntegerValueA = (someValue) => {
     return Number.isInteger(someValue) && Number.isFinite(someValue);
+}
+
+const isIntegerValueB = (someValue) => {
+    if (typeof (someValue) !== "number") {
+        return false;
+    }
+    return !Number.isNaN(someValue) && Number.parseInt(someValue) === someValue &&
+            !Number.isNaN(Number.parseInt(someValue, 10));
 }
 
 (function () {
 
-    console.log(isIntegerValue(23));
-    console.log(isIntegerValue(4e2));
-    console.log(isIntegerValue(NaN));
-    console.log(isIntegerValue(23.75));
-    console.log(isIntegerValue(-23));
+    console.log(isIntegerValueA(23));
+    console.log(isIntegerValueA(4e2));
+    console.log(isIntegerValueA(NaN));
+    console.log(isIntegerValueA(23.75));
+    console.log(isIntegerValueA(-23));
+
+    console.log("==================================");
+
+    console.log(isIntegerValueB(23));
+    console.log(isIntegerValueB(4e2));
+    console.log(isIntegerValueB(NaN));
+    console.log(isIntegerValueB(23.75));
+    console.log(isIntegerValueB(-23));
 
 })();
